b7d0b98205c1ddb67c88b1af0189a1226bbbef7d
kyungkoh <kyungkoh1328@naver.com>
2021-07-12 00:30:20 +0900

    jar로 프로젝트 재생성

---
+++ src/main/java/com/example/meteor/MeteorApplication.java
@@ -0,0 +1,13 @@
+package com.example.meteor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeteorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/meteor/MeteorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.meteor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteorApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello");
+    }
+
+}
