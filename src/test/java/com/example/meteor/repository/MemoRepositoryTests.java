0179e5f3dac1c5cf6f67529f5921b82c6ac6c91c
kyungkoh <kyungkoh1328@naver.com>
2021-07-25 20:03:09 +0900

    Paging 처리 테스트 코드 (Pageable Interface)

--- src/test/java/com/example/meteor/repository/MemoRepositoryTests.java
+++ src/test/java/com/example/meteor/repository/MemoRepositoryTests.java
@@ -4,9 +4,13 @@ import com.example.meteor.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import javax.transaction.Transactional;
 import java.util.stream.IntStream;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
 public class MemoRepositoryTests {
@@ -27,7 +31,7 @@ public class MemoRepositoryTests {
         });
     }*/
 
-   //객체를 조회하는 함수
+   //객체를 조회하는 함수 (Select)
    /* @Transactional
     @Test
     public void testSelect(){
@@ -37,17 +41,57 @@ public class MemoRepositoryTests {
         System.out.println(memo);
     }*/
 
-    // 100번의 memo 객체의 memo_text를 업데이트하는 함수
+    // 100번의 memo 객체의 memo_text를 업데이트하는 함수 (Update)
     /*@Test
     public void testUpdate(){
         Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
         System.out.println(memoRepository.save(memo));
     }*/
 
-    //100번째 객체 삭제 함수
-    @Test
+    //100번째 객체 삭제 함수 (Delete)
+    /*@Test
     public void testDelete(){
         Long mno = 100L;
         memoRepository.deleteById(mno);
+    }*/
+
+    //Paging 처리 함수
+    /*@Test
+    public void testPageDefault(){
+        //1페이지 10개
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+
+        System.out.println("-------------------------------");
+        System.out.println("Total pages: "+result.getTotalPages()); //총 몇페이지
+        System.out.println("Total Count: "+result.getTotalElements()); //전체 개수
+        System.out.println("Page Number: "+result.getNumber()); //현재 페이지 번호
+        System.out.println("Page Size: "+ result.getSize()); //페이지당 데이터 개수
+        System.out.println("has next page?: "+result.hasNext()); //다음 페이지 개수
+        System.out.println("first page?: "+result.isFirst()); //시작페이지(0) 여부
+
+        System.out.println("--------------------------------");
+        // 실제 페이지의 데이터 조회
+        for (Memo memo : result.getContent()){
+            System.out.println(memo);
+        }
+    } */
+
+    //정렬 조건 추가 (내림차순)
+    @Test
+    public void testSort(){
+        Sort sort1 = Sort.by("mno").descending();
+        Sort sort2 = Sort.by("memoText").ascending();
+        Sort sortAll = sort1.and(sort2);
+        //Pageable pageable = PageRequest.of(0,10,sort1);
+        Pageable pageable = PageRequest.of(0,20,sortAll);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo ->{
+            System.out.println(memo);
+        });
     }
-}
+
+    }
+
