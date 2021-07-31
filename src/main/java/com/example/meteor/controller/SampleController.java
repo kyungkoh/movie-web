bbbf6ae1eb8564222e41a0a6d0ac539bd7f6ac3b
kyungkoh <kyungkoh1328@naver.com>
2021-08-01 00:09:14 +0900

    thymeleaf 적용 예제

--- src/main/java/com/example/meteor/controller/SampleController.java
+++ src/main/java/com/example/meteor/controller/SampleController.java
@@ -1,12 +1,25 @@
 package com.example.meteor.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@Controller
+@RequestMapping("/sample")
+@Log4j2
 public class SampleController {
     @GetMapping("/hello")
     public String[] hello(){
         return new String[] {"Hello","World"};
     }
+
+    @GetMapping("/ex1")
+    public void ex1(){
+        log.info("ex1.........");
+    }
+
+
 }
