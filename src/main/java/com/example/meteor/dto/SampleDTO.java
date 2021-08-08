b96a6b4780a5b6733c518c77876d60afabdb134d
kyungkoh <kyungkoh1328@naver.com>
2021-08-08 17:47:06 +0900

    Sample DTO 타입의 객체 20개 추가

--- src/main/java/com/example/meteor/dto/SampleDTO.java
+++ src/main/java/com/example/meteor/dto/SampleDTO.java
@@ -1,4 +1,17 @@
 package com.example.meteor.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
 public class SampleDTO {
+
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+
 }
